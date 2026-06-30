f5f6458687936349125787907f40e16fdd0becb6
DennisNickens <Dennis@tp-llc.com>
2026-06-30 13:13:12 -0400

    Delete api/debug-variants.js

--- api/debug-variants.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function handler(req, res) {
-  const r = await fetch('https://api.printify.com/v1/shops/' + process.env.PRINTIFY_SHOP_ID + '/products/6a3fd211e7b8a9601e0f64cd.json', { headers: { Authorization: 'Bearer ' + process.env.PRINTIFY_API_TOKEN } });
-  const data = await r.json();
-  const colors = ['White', 'Pepper', 'Black'];
-  const filtered = (data.variants || []).filter(v => colors.some(c => v.title?.startsWith(c))).map(v => ({ id: v.id, title: v.title, enabled: v.is_enabled }));
-  res.json({ total: data.variants?.length, filtered });
-};
